875a781a012bcf5aea4dc8e9e58dac2027b68483
Victor Bala <savicsly@gmail.com>
2022-01-30 12:23:37 +0100

    Initial commit
    Signed-off-by: Victor Bala <savicsly@gmail.com>

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Kada Tech Summit</title>
+        <meta name="description" content="Kada tech summit" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Coming Soon...</h1>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default Home;
